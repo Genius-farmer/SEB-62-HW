b0d4336ac23cdfc698c0538d9d013f9beac861b6
Desmond Lim <desmond.lim@generalassemb.ly>
2026-05-05 08:58:27 +0800

    Add comments.

--- unit-2/8-create-langauge-registry-app-lab/lincoln/src/App.jsx
+++ unit-2/8-create-langauge-registry-app-lab/lincoln/src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Display from "./components/Display";
 
+// Desmond's comments
+// Good
+
 const queryClient = new QueryClient();
 
 function App() {
