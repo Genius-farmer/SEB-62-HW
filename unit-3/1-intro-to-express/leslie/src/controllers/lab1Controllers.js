0a0809c61b99684fa850d6af733d63e0ba1c9038
leslietan1981 <leslietan.yw1981@gmail.com>
2026-05-11 22:01:47 +0800

    Update with Lab1-3

--- unit-3/1-intro-to-express/leslie/src/controllers/lab1Controllers.js
+++ unit-3/1-intro-to-express/leslie/src/controllers/lab1Controllers.js
@@ -1,3 +1,5 @@
+import Collectibles from "../models/Lab1-3-model.js";
+
 export const getGreeting = (req, res) => {
   res.send(`Hello there, ${req.params.id}!`);
 };
@@ -8,5 +10,19 @@ export const getDiceRoll = (req, res) => {
     res.send(`You rolled a ${Math.floor(Math.random() * Number(req.params.max))}.`);
   } else {
     res.status(400).send("You must specify a number.");
+  }
+};
+
+export const getCollectible = (req, res) => {
+  if (req.params.index && req.params.index.trim().length > 0) {
+    const idx = Number(req.params.index);
+    if (idx < 0 || idx >= Collectibles.length) {
+      res.send("This item is not yet in stock. Check back soon!");
+    } else {
+      const collectibleFound = Collectibles[idx];
+      res.send(`The ${collectibleFound.name} is available at ${collectibleFound.price}.`);
+    }
+  } else {
+    res.send("You must specify an index number.");
   }
 };
--- unit-3/1-intro-to-express/leslie/src/routers/lab1Routers.js
+++ unit-3/1-intro-to-express/leslie/src/routers/lab1Routers.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getDiceRoll, getGreeting } from "../controllers/lab1Controllers.js";
+import { getCollectible, getDiceRoll, getGreeting } from "../controllers/lab1Controllers.js";
 
 const router = express.Router();
 
 router.get("/greetings/:id", getGreeting);
 router.get("/roll/:max", getDiceRoll);
+router.get("/collectibles/:index", getCollectible);
 
 export default router;
