4ab514415e3919cb0fbeb062583463144c201fda
leslietan1981 <leslietan.yw1981@gmail.com>
2026-05-12 00:24:59 +0800

    Update with Lab1-4

--- unit-3/1-intro-to-express/leslie/src/controllers/lab1Controllers.js
+++ unit-3/1-intro-to-express/leslie/src/controllers/lab1Controllers.js
@@ -1,8 +1,13 @@
 import Collectibles from "../models/Lab1-3-model.js";
+import Shoes from "../models/Lab1-4-model.js";
+
+// . Be Polite, Greet the User
 
 export const getGreeting = (req, res) => {
   res.send(`Hello there, ${req.params.id}!`);
 };
+
+// 2. Rolling the Dice
 
 export const getDiceRoll = (req, res) => {
   if (req.params.max && req.params.max.trim().length > 0) {
@@ -13,10 +18,14 @@ export const getDiceRoll = (req, res) => {
   }
 };
 
+// 3. I Want THAT One!
+
 export const getCollectible = (req, res) => {
   if (req.params.index && req.params.index.trim().length > 0) {
     const idx = Number(req.params.index);
-    if (idx < 0 || idx >= Collectibles.length) {
+    if (isNaN(idx)) {
+      res.send("You must specify an index number.");
+    } else if (idx < 0 || idx >= Collectibles.length) {
       res.send("This item is not yet in stock. Check back soon!");
     } else {
       const collectibleFound = Collectibles[idx];
@@ -24,5 +33,73 @@ export const getCollectible = (req, res) => {
     }
   } else {
     res.send("You must specify an index number.");
+  }
+};
+
+// 4. Filter Shoes by Query Parameters
+
+const checkQueryForErrors = (query) => {
+  const errorMsgs = [];
+  const minPrice = "min-price";
+  const maxPrice = "max-price";
+  const shoeType = "type";
+
+  if (
+    (minPrice in query && !(query[minPrice] && query[minPrice].trim().length > 0)) ||
+    (minPrice in query && isNaN(Number(query[minPrice])))
+  ) {
+    errorMsgs.push(`You must specify a number for ${minPrice}.`);
+  }
+
+  if (
+    (maxPrice in query && !(query[maxPrice] && query[maxPrice].trim().length > 0)) ||
+    (maxPrice in query && isNaN(Number(query[maxPrice])))
+  ) {
+    errorMsgs.push(`You must specify a number for ${maxPrice}.`);
+  }
+
+  return errorMsgs.length > 0 && errorMsgs;
+};
+
+const isNotSmaller = (minValue, targetValue) => {
+  return !(+targetValue < +minValue);
+};
+
+const isNotGreater = (maxValue, targetValue) => {
+  return !(+targetValue > +maxValue);
+};
+
+const isShoeType = (matchValue, targetValue) => {
+  return targetValue === matchValue;
+};
+
+export const getShoes = (req, res) => {
+  if (Object.keys(req.query).length === 0) {
+    res.send(Shoes);
+    return;
+  }
+
+  const criteria = {
+    "min-price": { checkFn: isNotSmaller, key: "price" },
+    "max-price": { checkFn: isNotGreater, key: "price" },
+    type: { checkFn: isShoeType, key: "type" },
+  };
+
+  const filteredQueryObj = Object.fromEntries(Object.entries(req.query).filter(([key]) => key in criteria));
+
+  if (Object.keys(filteredQueryObj).length === 0) {
+    res.status(400).send("No valid parameters were found in query");
+  } else {
+    const queryErrors = checkQueryForErrors(filteredQueryObj);
+
+    if (queryErrors) {
+      res.status(400).send(queryErrors.join(" "));
+    } else {
+      const result = Shoes.filter((shoe) =>
+        Object.entries(filteredQueryObj).every(([key, value]) => criteria[key].checkFn(value, shoe[criteria[key].key])),
+      );
+
+      res.send(result);
+    }
   }
 };
